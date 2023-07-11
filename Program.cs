e850906b19d140c429ce288cf43c0e0519a7a2a2
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-11 12:30:40 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gotoexample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value:{0}",i);
+            }
+            end: Console.WriteLine("the emd");
+        }
+    }
+}
